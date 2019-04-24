341a2d6a1c261fddb7191a275368d2863e4122f5
Ying-Lenny <owenlenihan101@gmail.com>
2019-04-24 19:13:00 +0100

    Initial commit

---
+++ app/src/main/java/org/wit/gameinfo/GameInfoActivity.kt
@@ -0,0 +1,12 @@
+package org.wit.gameinfo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GameInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gameinfo)
+    }
+}
